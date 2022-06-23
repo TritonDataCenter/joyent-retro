83a2cf46e578471641f9a5add43eb5c1b2d9f1a7
Jonathan Perkin <jonathan@perkin.org.uk>
2022-06-23 12:22:34 +0100

    Ensure SSL CA bundle is always copied.

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@
 
 #
 # Copyright (c) 2017, Joyent, Inc.
+# Copyright (c) 2022 MNX Cloud, Inc.
 #
 
 #
@@ -83,7 +84,6 @@ fi
 if [[ ${ca_cert_age} -gt 86400 ]]; then
     # CA certs so curl https:// can work
     echo "=> Grabbing latest CA certs... (current is ${ca_cert_age}s old)"
-    mkdir -p custom/etc/ssl/certs
     curl -o data/ca-bundle.crt.new ${CERT_URL}
     if [[ $? -eq 0 && -s data/ca-bundle.crt.new ]]; then
         mv data/ca-bundle.crt.new data/ca-bundle.crt
@@ -97,8 +97,9 @@ if [[ ${ca_cert_age} -gt 86400 ]]; then
         echo "** Failed to get CA bundle (too small)"
         exit 1
     fi
-    cp ./data/ca-bundle.crt custom/etc/ssl/ca-bundle.crt
 fi
+mkdir -p custom/etc/ssl/certs
+cp ./data/ca-bundle.crt custom/etc/ssl/ca-bundle.crt
 
 EXCLUDES=
 # New mdata-get (that works in the GZ) was added with TOOLS-292 in Oct 2013.
